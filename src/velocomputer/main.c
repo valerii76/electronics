518c858c47f10e2a47772125593d5740e8644881
Valery Volgutov <valerii76@gmail.com>
2012-09-24 23:30:35 +0400

    add twi (atmel appnote)

--- src/velocomputer/main.c
+++ src/velocomputer/main.c
@@ -21,14 +21,13 @@
 #include "config.h"
 
 #include "nokia_1202.h"
-/*#include "math/fixed.h"*/
-#include "dbg_flash.h"
+/*#include "dbg_flash.h"*/
 #include "dbg_uart.h"
 
 #include <util/delay.h>
 
 #include "font_5thagent12.h"
-#include "font_5thagent24.h"
+/*#include "font_5thagent24.h"*/
 
 int main (int argc, char *argv[])
 {
@@ -37,23 +36,17 @@ int main (int argc, char *argv[])
     lcd_init();
     lcd_clr();
 
-    /*lcd_set_font (font_5thagent12n, FONT_5THAGENT12N_NUM_GLYPS);*/
-    /*lcd_string_pgm (PSTR ("0123456789"), (uint8_t) 0, (uint8_t) 0);*/
+    lcd_set_font (font_5thagent12, FONT_5THAGENT12_NUM_GLYPS);
+    lcd_string_pgm (PSTR ("0123456789"), (uint8_t) 0, (uint8_t) 0);
 
-    /*lcd_set_font (font_5thagent24n, FONT_5THAGENT24N_NUM_GLYPS);*/
+    /*lcd_set_font (font_5thagent24, FONT_5THAGENT24_NUM_GLYPS);*/
     /*lcd_string_pgm (PSTR ("9789"), (uint8_t) 1, (uint8_t) 0);*/
 
-    /*lcd_set_font (font_5thagent12c, FONT_5THAGENT12C_NUM_GLYPS);*/
+    /*lcd_set_font (font_5thagent12, FONT_5THAGENT12_NUM_GLYPS);*/
     /*lcd_string_pgm (PSTR ("ABCD"), (uint8_t) 3, (uint8_t) 0);*/
 
-    /*lcd_set_font (font_5thagent24c, FONT_5THAGENT24C_NUM_GLYPS);*/
+    /*lcd_set_font (font_5thagent24, FONT_5THAGENT24_NUM_GLYPS);*/
     /*lcd_string_pgm (PSTR ("ABCD"), (uint8_t) 4, (uint8_t) 0);*/
-
-    lcd_set_font (font_5thagent12, FONT_5THAGENT12_NUM_GLYPS);
-    lcd_string_pgm (PSTR ("VALERII76:.,+-"), (uint8_t) 0, (uint8_t) 0);
-
-    lcd_set_font (font_5thagent24, FONT_5THAGENT24_NUM_GLYPS);
-    lcd_string_pgm (PSTR (":+-.,"), (uint8_t) 4, (uint8_t) 0);
 
     return 0;
 }
